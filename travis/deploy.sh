73cc859a5a11d08e0d5f3fb3a5f8c7f5de50a8a1
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2019-12-26 11:03:01 -0500

    Update deploy.sh
    add push of readme

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -35,3 +35,25 @@ docker manifest push ${DOCKER_USER}/${DOCKER_NAME}:${DOCKER_TAG}
 
 # Display docker manifest list
 docker manifest inspect ${DOCKER_USER}/${DOCKER_NAME}:${DOCKER_TAG}
+
+# Push the Readme File
+local README_NAME="README.md"
+local token=$(curl -s -X POST \
+-H "Content-Type: application/json" \
+-d '{"username": "'"${DOCKER_USER}"'", "password": "'"${DOCKER_PASS}"'"}' \
+https://hub.docker.com/v2/users/login/ | jq -r .token)
+
+local code=$(jq -n --arg msg "$(<${README_NAME})" \
+'{"registry":"registry-1.docker.io","full_description": $msg }' | \
+    curl -s -o /dev/null  -L -w "%{http_code}" \
+       https://hub.docker.com/v2/repositories/"${DOCKER_USER}"/"${DOCKER_NAME}"/ \
+       -d @- -X PATCH \
+       -H "Content-Type: application/json" \
+       -H "Authorization: JWT ${token}")
+
+if [[ "${code}" = "200" ]]; then
+printf "Successfully pushed %s to Docker Hub\n" "${README_NAME}"
+else
+printf "Unable to push %s to Docker Hub, the response code: %s\n" "${README_NAME}" "${code}"
+exit 1
+fi
