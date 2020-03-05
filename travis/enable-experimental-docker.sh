3f08cba6fea36fc2c7e59980db805e69c9cc7cfa
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2020-03-04 20:59:29 -0500

    fix sudo

--- travis/enable-experimental-docker.sh
+++ travis/enable-experimental-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
-mkdir $HOME/.docker
-touch $HOME/.docker/config.json
+sudo mkdir $HOME/.docker
+sudo touch $HOME/.docker/config.json
 echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
 sudo service docker restart
